9b3d5919db8ad4d4baa4cda4f013b7f5b41fd6f3
peksinsara <peksin.sara@gmail.com>
2021-12-16 11:43:42 +0100

    navigation bar full functionality added

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,10 +1,53 @@
 import * as React from 'react';
-import {View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
 
+//Screen names
+const homeName = "Home";
+const detailsName = "Details";
 
-export default function MainContainer(){
-    return(
-        <View></View>
-    )
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === detailsName) {
+              iconName = focused ? 'list' : 'list-outline';
+
+            } 
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'black',
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 80}
+        }}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={detailsName} component={DetailsScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default MainContainer;
