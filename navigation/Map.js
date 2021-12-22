13a799655bf10f045481e35cf290bdc0bdb7d54a
peksinsara <peksin.sara@gmail.com>
2021-12-22 17:18:11 +0100

    added API with valid data

--- navigation/Map.js
+++ navigation/Map.js
@@ -1,72 +1,73 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import MapView, { PROVIDER_GOOGLE,Marker } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: '100%',
+    height: "100%",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
 });
 
-
-export default class MapScreen extends React.Component{
-
-  constructor(props){
-    super(props);
-    
-    this.state = {
-      markers: [{
-        title: 'Sarajevo',
-        coordinates: {
-          latitude: 43.86666667,
-          longitude:  18.416667,
-          latitudeDelta: 0.0335,
-            longitudeDelta: 0.04250
-        },
-      },
-      {
-        title: 'Vienna',
-        coordinates: {
-          latitude: 44.20169,
-          longitude: 17.90397,
-          latitudeDelta: 0.0335,
-            longitudeDelta: 0.04250
-        },  
-      }]
-    };
+export default class MapScreen extends React.Component {
+  state = {
+    aqi: " ",
+    location: "",
+    lat: 0,
+    long: 0,
+  };
+  /*https://api.waqi.info/feed/sarajevo/?token=2ec4dd778796b645fcba62b587874b25e477acd9*/
+  componentDidMount() {
+    const location = "sarajevo";
+    const token = "2ec4dd778796b645fcba62b587874b25e477acd9";
+    fetch(`https://api.waqi.info/feed/${location}/?token=${token}`)
+      .then((res) => res.json())
+      .then((fetchedData) => {
+        this.setState({
+          location: fetchedData.data.city.name,
+          aqi: String(fetchedData.data.aqi),
+          lat: fetchedData.data.city.geo[0],
+          long: fetchedData.data.city.geo[1],
+        });
+      });
   }
- 
-render(){
-return(
-<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => navigation.navigate("Home")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Map Screen
-      </Text>
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          initialRegion={{
-            latitude: 43.86666667,
-          longitude:  18.416667,
-          latitudeDelta: 0.0835,
-            longitudeDelta: 0.09250
-          }}
-        >
-           {this.state.markers.map((marker, index) => ( 
-              <Marker key={index} coordinate={marker.coordinates} title={marker.title} 
-               /> ))}	
-        </MapView>
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text
+          onPress={() => navigation.navigate("Home")}
+          style={{ fontSize: 26, fontWeight: "bold" }}
+        ></Text>
+
+        <View style={styles.container}>
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            initialRegion={{
+              latitude: 43.86666667,
+              longitude: 18.416667,
+              latitudeDelta: 0.0835,
+              longitudeDelta: 2.3,
+            }}
+          >
+            <Marker
+              key={Math.random()}
+              coordinate={{
+                latitude: this.state.lat,
+                longitude: this.state.long,
+              }}
+              title={this.state.location}
+              description={this.state.aqi}
+            ></Marker>
+          </MapView>
+
+
+        </View>
       </View>
-    </View>
-)
-}
- 
+    );
+  }
 }
